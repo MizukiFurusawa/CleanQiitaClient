14b2e1e5f3ae825d1745d3e04fe8b50a3651576b
MizukiFurusawa <fmzk0326@gmail.com>
2017-10-07 18:08:01 +0900

    ビルドできるようにした.

--- Utility/AuthInfo.swift
+++ Utility/AuthInfo.swift
@@ -10,8 +10,8 @@ import Foundation
 
 public struct AuthInfo {
     
-    public static let clientId: String = "hoge"
-    public static let clientSecret: String = "hoge"
+    public static let clientId: String = "200986e4e3fa243511b6b8510a377495ab71d071"
+    public static let clientSecret: String = "0ac89b0e45dc11e39e3ac5cc5f9d48112849b9f0"
     public static let accessTokenNotificationKey: String = "AccessTokenNotificationKey"
     public static let accessTokenState: String = "F355968FDEB05B26"
     public static let redirectUrlScheme: String = "clean-qiita-client"
